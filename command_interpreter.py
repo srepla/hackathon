5e0f45e71ffab201ae6b7edfbd4edec06c8121cb
Christopher Alpers <christopher.alpers@adsoul.com>
2022-07-06 17:08:20 +0200

    dht bla bla

--- command_interpreter.py
+++ command_interpreter.py
@@ -2,6 +2,7 @@ class CommandInterpreter:
 
     @staticmethod
     def process_command(commands):
-        commands = [command.lower for command in commands]
-        if "temperatur" in commands or "warm" in commands or "kalt" in commands:
-            return 0
+        return 0
+        # commands = [command.lower for command in commands]
+        # if "temperatur" in commands or "warm" in commands or "kalt" in commands:
+        #     return 0
--- temp_hum_reader.py
+++ temp_hum_reader.py
@@ -1,24 +1,14 @@
 import time
+import board
+import adafruit_dht
 
 
 class TempHumReader:
-    # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-    # SPDX-License-Identifier: MIT
-
-    import time
-    import board
-    import adafruit_dht
-
-    # Initial the dht device, with data pin connected to:
-    dhtDevice = adafruit_dht.DHT22(board.D4)
-
-    # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-    # This may be necessary on a Linux single board computer like the Raspberry Pi,
-    # but it will not work in CircuitPython.
-    # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
     @staticmethod
     def read():
+        dhtDevice = adafruit_dht.DHT22(board.D4)
+
         for i in range(5):
             try:
                 # Print the values to the serial port
