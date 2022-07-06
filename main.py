8fc0928fad175365227d92560bf63d12747e6ff8
Christopher Alpers <christopher.alpers@adsoul.com>
2022-07-06 14:38:21 +0200

    Add everything we have done so far

---
+++ main.py
@@ -0,0 +1,38 @@
+import speech_recognition as sr
+import sounddevice as sd
+import numpy as np
+from gtts import gTTS
+from playsound import playsound
+
+# speech_engine = sr.Recognizer()
+
+
+# def print_sound(indata, outdata, frames, time, status):
+#     volume_norm = np.linalg.norm(indata) * 10
+#     print("|" * int(volume_norm))
+#
+#
+# def from_microphone():
+#     with sr.Microphone() as micro:
+#         print("Recording...")
+#         audio = speech_engine.record(micro, duration=5)
+#         print("Recognition...")
+#         text = speech_engine.recognize_google(audio, language="de-DE")
+#         return text
+
+
+
+
+
+
+if __name__ == '__main__':
+    # print(from_microphone())
+    # with sd.Stream(callback=print_sound):
+    #     sd.sleep(10000)
+
+    language = 'de'
+    tts = gTTS(text="Es ist arsch kalt! So kann man nicht arbeiten!",
+               lang=language,
+               slow=False)
+    tts.save("./res/zu_kalt.mp3")
+    playsound("./res/zu_kalt.mp3")
