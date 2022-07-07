232c207fff9b7ec0dcd60d9fc6a20f8e31c21add
Christopher Alpers <christopher.alpers@adsoul.com>
2022-07-07 15:37:03 +0200

    nsfw kram

--- nsfw_detector.py
+++ nsfw_detector.py
@@ -6,7 +6,7 @@ class NSFWDetector:
     def __init__(self):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(11, GPIO.IN)
 
     def get_nsfw_state(self):
         return GPIO.input(22) == GPIO.HIGH
