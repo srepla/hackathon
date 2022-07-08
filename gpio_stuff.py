12d5e625bb976d1c90e357a3ceb87ca369f56505
Christopher Alpers <christopher.alpers@adsoul.com>
2022-07-08 12:00:11 +0200

    thread kram

--- gpio_stuff.py
+++ gpio_stuff.py
@@ -9,20 +9,20 @@ class GPIOStuff:
     def __init__(self):
         if platform.system() == "Linux":
             GPIO.setwarnings(False)
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-            GPIO.setup(11, GPIO.IN)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(17, GPIO.IN)
 
     def is_recording(self):
         if platform.system() == "Linux":
-            GPIO.output(8, GPIO.HIGH)
+            GPIO.output(14, GPIO.HIGH)
 
     def finished_recording(self):
         if platform.system() == "Linux":
-            GPIO.output(8, GPIO.LOW)
+            GPIO.output(14, GPIO.LOW)
 
     def get_nsfw_state(self):
         if platform.system() == "Linux":
-            return GPIO.input(11) == 1
+            return GPIO.input(17) == 1
         else:
             return False
